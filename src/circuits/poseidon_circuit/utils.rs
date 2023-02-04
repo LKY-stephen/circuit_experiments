10589c1f514f05eba991de6a66b30e14a6a3665d
Keyang.Liu <stephenkobylky@gmail.com>
2023-02-04 13:32:23 +0900

    implement poseidon hash

---
+++ src/circuits/poseidon_circuit/utils.rs
@@ -0,0 +1,34 @@
+use halo2_proofs::arithmetic::FieldExt;
+use std::fmt;
+
+/// The type used to hold the MDS matrix and its inverse.
+pub type Mds<F, const WIDTH: usize> = [[F; WIDTH]; WIDTH];
+
+/// A specification for a Poseidon permutation.
+/// The input should be a field F
+/// the sponge width is WIDTH
+/// Number of full round and partial rounds are fixed
+pub trait Spec<F: FieldExt, const WIDTH: usize>: fmt::Debug {
+    /// The number of full rounds for this specification.
+    ///
+    /// This must be an even number.
+    fn full_rounds() -> usize;
+
+    /// The number of partial rounds for this specification.
+    fn partial_rounds() -> usize;
+
+    /// Generates `mds` corresponding to this specification.
+    fn mds() -> Mds<F, WIDTH>;
+
+    /// Generates `ARKS` corresponding to this specification.
+    fn arks() -> Vec<[F; WIDTH]>;
+
+    // Generate the capacity
+    fn capacity() -> u128;
+
+    // Return the Pad Element;
+    fn pad() -> Vec<F>;
+
+    // fixed rounds for squeezing.
+    fn squeeze_rounds() -> usize;
+}
